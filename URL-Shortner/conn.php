b8afbcc3948f043138c54cc7f9a79657b9d0d7e4
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-03-04 01:59:30 +0300

    starting url shortner mini project

---
+++ URL-Shortner/conn.php
@@ -0,0 +1,19 @@
+<?php
+
+    require_once __DIR__ . 'vendor/autoload.php';
+
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
+    $db_host = $_ENV['DB_HOST'];
+    $db_user = $_ENV['DB_USER'];
+    $db_pass = $_ENV['DB_PASSWORD'];
+    $db_name = $_ENV['DB_NAME'];
+
+    try {
+        $conn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        echo("error: ". $e->getMessage());
+    }
+?>
